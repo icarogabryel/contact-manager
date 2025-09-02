615dbb01978848c5d057490fa018164b3347513f
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-09-02 12:19:47 -0300

    feat: implement contact creation and put everything in url

--- config/urls.py
+++ config/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+
+from main import views
+
+
+urlpatterns = [
+    path('', views.contact_list, name='contact_list'),
+    path('new', views.contact_create, name='contact_create')
+]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .forms import ContactForm
 from .models import Contact
 
 
 def contact_list(request):
     contacts = Contact.objects.all()
 
-    return render(request, 'main/contact_list.html', {'contacts': contacts})
+    return render(request, 'contact_list.html', {'contacts': contacts})
+
+
+def contact_create(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_list')
+
+
+    return render(request, 'contact_create.html', {'form': ContactForm()})
