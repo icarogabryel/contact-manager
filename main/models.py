6fa4afe9948bca622164a01ce5b0e379a9cd3f50
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-09-02 11:51:07 -0300

    feat: make migration and add database to .gitignore

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+    birthday = models.DateField()
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
